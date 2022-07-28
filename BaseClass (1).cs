3749b10e51a8d501c6f3da7a40e2c2bca122cef2
shitalshinde0702 <110106093+shitalshinde0702@users.noreply.github.com>
2022-07-28 17:10:26 +0530

    Add files via upload
    base file

---
+++ BaseClass (1).cs
@@ -0,0 +1,90 @@
+﻿using System;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using Serilog;
+using Serilog.Core;
+using OpenQA.Selenium.Firefox;
+
+namespace Demo2.Utilities
+{
+    [Binding]
+    public class BaseClass
+    {
+        public static  IWebDriver driver;
+        public static ExtentReports extents;
+        public static ExtentTest feature;
+        public static ExtentTest scenario;
+
+
+        [BeforeFeature]
+
+        public static void featureBrowser(FeatureContext featureContext)
+        {
+            feature = extents.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+            Log.Information("selecting feature file {0} to run", featureContext.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        public static void OpenBrowser(ScenarioContext scenarioContext)
+        {
+            scenario = feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
+            Log.Information("selecting scenario {0} to run", scenarioContext.ScenarioInfo.Title);
+
+            driver = new FirefoxDriver();
+        }
+        [BeforeTestRun]
+        public static void GenerateReport()
+        {
+            var htmlReport = new ExtentHtmlReporter(@"C:\Users\mindc1may270\source\repos\Demo2\Index.html");
+            htmlReport.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Standard;
+            extents = new ExtentReports();
+            extents.AttachReporter(htmlReport);
+            LoggingLevelSwitch loggingLevel = new LoggingLevelSwitch(Serilog.Events.LogEventLevel.Debug);
+            Log.Logger = new LoggerConfiguration().MinimumLevel.ControlledBy(levelSwitch: loggingLevel).WriteTo.File(
+                @"C:\Users\mindc1may270\source\repos\Demo2\logs", outputTemplate: "{TimeSpan:yyyy-MM-dd HH:mm:ss.fff} | {Level:u3} | {Message}{NewLine}", rollingInterval: RollingInterval.Day).CreateLogger();
+        }
+
+        [AfterTestRun]
+        public static void CloseExtentReport()
+        {
+            extents.Flush();
+        }
+
+        [AfterStep]
+        public static void InsertReportingSteps(ScenarioContext scenarioContext)
+        {
+            if (scenarioContext.TestError == null)
+            {
+                var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+
+            }
+            if (scenarioContext.TestError != null)
+            {
+                var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+                if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(scenarioContext.TestError.Message);
+            }
+        }
+
+
+        [AfterScenario]
+
+        public static void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
